cfc9c9cb615db243a6210f2d7438733bb0fbddc7
boyyang-love <boyyang-love@outlook.com>
2020-12-05 21:43:29 +0800

    'yang'

--- src/api/https.ts
+++ src/api/https.ts
@@ -9,21 +9,11 @@ import qs from 'qs'
  * @debug  测试环境
  */
 
-// if (process.env.NODE_ENV == 'development') {
-//     baseUrl = ''
-// } else if (process.env.NODE_ENV == 'production') {
-//     baseUrl = ''
-// } else if (process.env.NODE_ENV == 'debug') {
-//     baseUrl = ''
-// }
-
-
-// axios.defaults.baseURL = 'http://1905.com'
-
 const server: AxiosInstance = axios.create({
     baseURL: process.env.NODE_ENV == 'development' ? 'http://1905.com': 'http://2020.com' ,
     timeout: 3000,
 })
+
 /**
  * axios请求拦截
  * @return 
